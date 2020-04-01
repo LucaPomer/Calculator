30b392234d693b5e4c32b92afb16d22dc1ff1def
luca pomer <ucapomer@gmail.com>
2020-04-01 16:25:35 +0200

    lifted number state up to calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,32 +1,72 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNumChangeLeft=this.handleNumChangeLeft.bind(this);
+        this.handleNumChangeRight=this.handleNumChangeRight.bind(this);
+        this.state = {
+            numLeft: 0,
+            numRight: 0
+        }
+
+    }
+
     render() {
+        const numLeft = this.state.numLeft;
+        const numRight = this.state.numRight;
         return (
             <div>
-                <NumberInput/>
+                <NumberInput
+                    numSide="l"
+                    handleNumChange={this.handleNumChangeLeft}
+                    numValue={numLeft}/>
+                <Button variant="primary" onClick={this.addOperation}>+</Button>
+                <NumberInput
+                    numSide="r"
+                    handleNumChange={this.handleNumChangeRight}
+                    numValue={numRight}/>
             </div>
 
         )
     }
+
+    handleNumChangeLeft(newNum) {
+        console.log(newNum);
+        this.setState({numLeft: newNum});
+    }
+
+    handleNumChangeRight(newNum) {
+        this.setState({numRight: newNum});
+    }
+
+    addOperation() {
+        console.log("add ");
+    }
 }
+
+const numSide = {
+    l: 'left',
+    r: 'right'
+};
 
 class NumberInput extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        console.log("new value " + event.target.value)
-        this.setState({value: event.target.value});
+        this.props.handleNumChange(event.target.value.replace(/[^0-9]/g, ''));
+        //  this.setState({value: event.target.numberValue.replace(/[^0-9]/g, '')});
     }
 
     render() {
-        return(
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            );
+        const numValue = this.props.numValue;
+        return (
+            <input type="text" value={numValue} onChange={this.handleChange}/>
+        );
     }
 
 }
