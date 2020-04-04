2cec22474d98e1a8f4ec836623202b634fd69bab
luca pomer <ucapomer@gmail.com>
2020-04-04 12:35:28 +0200

    added icons to buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ListGroupItem from "react-bootstrap/ListGroupItem";
-import {all, create} from 'mathjs'
 import Row from "react-bootstrap/Row";
 import NumberInput from './NumberInput.js';
 import CalcHistory from "./CalcHistory";
 import Col from "react-bootstrap/esm/Col";
+
 
 class Calculator extends React.Component {
     constructor(props) {
@@ -28,35 +28,42 @@ class Calculator extends React.Component {
         const numRight = this.state.numRight;
         return (
             <div>
-                <h1>Calculator</h1>
-                <Row id={"mainRow"} className="justify-content-md-center">
-                    <Col className={"calcArea"} xs lg="3">
-                        <h2>Input</h2>
-                        <NumberInput
-                            numSide="l"
-                            handleNumChange={this.handleNumChangeLeft}
-                            numValue={numLeft}/>
-                        <NumberInput
-                            numSide="r"
-                            handleNumChange={this.handleNumChangeRight}
-                            numValue={numRight}/>
-                        <Row className={"buttonRow"}>
-                            <Button variant="outline-dark" onClick={this.addOperation}>+</Button>
-                            <Button variant="outline-dark" onClick={() => this.decreaseOperation()}>-</Button>
-                            <Button variant="outline-dark" onClick={() => this.multiplyOperation()}>*</Button>
-                            <Button variant="outline-dark" onClick={() => this.rootOperation()}>root</Button>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"/>
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/v4-shims.css"/>
+                        <h1>Calculator</h1>
+                        <Row id={"mainRow"} className="justify-content-md-center">
+                            <Col className={"calcArea"} xs lg="3">
+                                <h2>Input</h2>
+                                <NumberInput
+                                    numSide="l"
+                                    handleNumChange={this.handleNumChangeLeft}
+                                    numValue={numLeft}/>
+                                <NumberInput
+                                    numSide="r"
+                                    handleNumChange={this.handleNumChangeRight}
+                                    numValue={numRight}/>
+                                <Row className={"buttonRow"}>
+                                    <Button variant="outline-dark" onClick={this.addOperation}><i
+                                        className="fas fa-plus"/></Button>
+                                    <Button variant="outline-dark" onClick={() => this.decreaseOperation()}><i
+                                        className="fas fa-minus"/></Button>
+                                    <Button variant="outline-dark" onClick={() => this.multiplyOperation()}><i
+                                        className="fa fa-close"/></Button>
+                                    <Button variant="outline-dark" onClick={() => this.rootOperation()}><i
+                                        className="fa fa-square-root-alt"/></Button>
+                                </Row>
+                                <ListGroupItem variant="light">Calculation
+                                    : {this.state.calculationString}</ListGroupItem>
+                                <ListGroupItem variant="light">Result : {this.state.result}</ListGroupItem>
+                            </Col>
+                            <Col className={"historyArea"} xs lg="3">
+                                <h2>History</h2>
+                                <CalcHistory calcHistoryItems={this.state.calcHistoryList}/>
+                            </Col>
                         </Row>
-                        <ListGroupItem variant="light">Calculation : {this.state.calculationString}</ListGroupItem>
-                        <ListGroupItem variant="light">Result : {this.state.result}</ListGroupItem>
-                    </Col>
-                    <Col className={"historyArea"} xs lg="3">
-                        <h2>History</h2>
-                        <CalcHistory calcHistoryItems={this.state.calcHistoryList}/>
-                    </Col>
-                </Row>
             </div>
 
-        )
+    )
     }
 
     handleNumChangeLeft(newNum) {
@@ -69,10 +76,10 @@ class Calculator extends React.Component {
 
     checkIfNum(input) {
         if (isNaN(input)) {
-            return false
-        } else {
-            return true;
-        }
+        return false
+    } else {
+        return true;
+    }
     }
 
     addOperation() {
@@ -82,23 +89,23 @@ class Calculator extends React.Component {
         let newResult;
         let calcHistoryItemsCopy = this.state.calcHistoryList;
         if (this.checkIfNum(numLeft) && this.checkIfNum(numRight)) {
-            newResult = parseInt(numLeft) + parseInt(numRight);
-            newCalculation = ` ${numLeft} + ${numRight} = ${newResult}`;
-        } else {
-            newCalculation = 'not posiible -> invalid input';
-            newResult = " ";
-        }
+        newResult = parseInt(numLeft) + parseInt(numRight);
+        newCalculation = ` ${numLeft} + ${numRight} = ${newResult}`;
+    } else {
+        newCalculation = 'not posiible -> invalid input';
+        newResult = " ";
+    }
 
         calcHistoryItemsCopy.push(newCalculation);
         if (calcHistoryItemsCopy.length > 6) {
-            calcHistoryItemsCopy.shift();
-        }
+        calcHistoryItemsCopy.shift();
+    }
 
         this.setState({
-            calculationString: newCalculation,
-            result: newResult,
-            calcHistoryList: calcHistoryItemsCopy,
-        });
+        calculationString: newCalculation,
+        result: newResult,
+        calcHistoryList: calcHistoryItemsCopy,
+    });
     }
 
     decreaseOperation() {
@@ -108,20 +115,20 @@ class Calculator extends React.Component {
         let newResult;
         let calcHistoryItemsCopy = this.state.calcHistoryList;
         if (this.checkIfNum(numLeft) && this.checkIfNum(numRight)) {
-            newResult = parseInt(numLeft) - parseInt(numRight);
-            newCalculation = ` ${numLeft} - ${numRight} = ${newResult} `;
-        } else {
-            newCalculation = 'not posiible -> invalid input';
-            newResult = " ";
-        }
+        newResult = parseInt(numLeft) - parseInt(numRight);
+        newCalculation = ` ${numLeft} - ${numRight} = ${newResult} `;
+    } else {
+        newCalculation = 'not posiible -> invalid input';
+        newResult = " ";
+    }
         calcHistoryItemsCopy.push(newCalculation);
         if (calcHistoryItemsCopy.length > 6) {
-            calcHistoryItemsCopy.shift();
-        }
+        calcHistoryItemsCopy.shift();
+    }
         this.setState({
-            calculationString: newCalculation,
-            result: newResult
-        });
+        calculationString: newCalculation,
+        result: newResult
+    });
     }
 
     multiplyOperation() {
@@ -131,21 +138,21 @@ class Calculator extends React.Component {
         let newResult;
         let calcHistoryItemsCopy = this.state.calcHistoryList;
         if (this.checkIfNum(numLeft) && this.checkIfNum(numRight)) {
-            newResult = parseInt(numLeft) * parseInt(numRight);
-            newCalculation = ` ${numLeft} * ${numRight} = ${newResult} `;
+        newResult = parseInt(numLeft) * parseInt(numRight);
+        newCalculation = ` ${numLeft} * ${numRight} = ${newResult} `;
 
-        } else {
-            newCalculation = 'not posiible -> invalid input';
-            newResult = " ";
-        }
+    } else {
+        newCalculation = 'not posiible -> invalid input';
+        newResult = " ";
+    }
         calcHistoryItemsCopy.push(newCalculation);
         if (calcHistoryItemsCopy.length > 6) {
-            calcHistoryItemsCopy.shift();
-        }
+        calcHistoryItemsCopy.shift();
+    }
         this.setState({
-            calculationString: newCalculation,
-            result: newResult
-        });
+        calculationString: newCalculation,
+        result: newResult
+    });
     }
 
 
@@ -156,36 +163,34 @@ class Calculator extends React.Component {
         let newResult;
         let calcHistoryItemsCopy = this.state.calcHistoryList;
         if (this.checkIfNum(numLeft) && this.checkIfNum(numRight)) {
-            if (numRight <= 0) {
-                newCalculation = ` ${numLeft} root ${numRight} not possible `;
-                newResult = 'root must be non zero';
-            } else {
-                const config = {};
-                const math = create(all, config);
-                newResult = math.sqrt(parseInt(numRight));
-                newResult*=parseInt(numLeft);
-                newCalculation = ` ${numLeft} root ${numRight} = ${newResult}`;
-            }
-        } else {
-            newCalculation = 'not possible -> invalid input';
-            newResult = " ";
-        }
+        if (numRight <= 0) {
+        newCalculation = ` ${numLeft} root ${numRight} not possible `;
+        newResult = 'root must be non zero';
+    } else {
+        newResult = Math.sqrt(parseInt(numRight));
+        newResult*=parseInt(numLeft);
+        newCalculation = ` ${numLeft} root ${numRight} = ${newResult}`;
+    }
+    } else {
+        newCalculation = 'not possible -> invalid input';
+        newResult = " ";
+    }
         calcHistoryItemsCopy.push(newCalculation);
         if (calcHistoryItemsCopy.length > 6) {
-            calcHistoryItemsCopy.shift();
-        }
+        calcHistoryItemsCopy.shift();
+    }
 
         this.setState({
-            calculationString: newCalculation,
-            result: newResult
-        });
+        calculationString: newCalculation,
+        result: newResult
+    });
     }
-}
+    }
 
-const numSide = {
-    l: 'left',
-    r: 'right'
-};
+    const numSide = {
+        l: 'left',
+        r: 'right'
+    };
 
 
-export default Calculator;
+    export default Calculator;
